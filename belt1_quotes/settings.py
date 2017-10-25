212c0bf9d966b5203671c56bfec6c078493afc8f
Staci Martinez <staci@Stacis-MacBook-Pro.local>
2017-10-25 12:50:29 -0500

    updated password

--- belt1_quotes/settings.py
+++ belt1_quotes/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'd!i7vx*t9ah@+x^%+@b(kiwcru@=g#+4fv*2s=n8#3up8reypa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['18.216.112.108']
 
 
 # Application definition
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
